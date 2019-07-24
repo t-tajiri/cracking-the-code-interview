4e5a745d11355a67bc1dfb2ed3a1488c1c5b7285
t-tajiri <swallow.ava@gmail.com>
2019-07-25 00:18:18 +0900

    solve a problem that checks non dupulicate character in text

--- chapter1/src/Main.java
+++ chapter1/src/Main.java
@@ -1,5 +1,30 @@
 public class Main {
+    private static final int ASCII_CODE_LENGTH = 128;
+
+    private static boolean isUniqueCharacter(String str) {
+        if(str.length() > ASCII_CODE_LENGTH) {
+            return false;
+        }
+
+        var used = new boolean[ASCII_CODE_LENGTH];
+        for(int i = 0; i < str.length(); i++) {
+            var target = str.charAt(i);
+
+            if(used[target]) {
+                return false;
+            }
+
+            used[target] = true;
+        }
+
+        return true;
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        String str = "main";
+        System.out.println(str + " is duplicated character? : " + isUniqueCharacter(str));
+
+        String duplicate = "testDuplicate";
+        System.out.println(duplicate + " is duplicated character? : " + isUniqueCharacter(duplicate));
     }
 }
